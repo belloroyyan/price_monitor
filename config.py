3d59258a3b3e0f6ef7e65973efe3ffc99f4d590f
kato彡 <belloroyyan@gmail.com>
2026-06-12 02:02:33 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -3,22 +3,27 @@ TARGET_PRODUCTS = [
         "name" : "A Light In The Attic",
         "url": "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html",
         "selector_price": "p.price_color",
-        "selector_stock": "p.instock.availability"
+        "selector_stock": "p.instock.availability",
+        "threshold" : 52.00
     },
     {
         "name": "Tipping the Velvet",
         "url": "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html",
         "selector_price": "p.price_color",
-        "selector_stock": "p.instock.availability"
+        "selector_stock": "p.instock.availability",
+        "threshold" : 54.00
     },
     {
         "name": "Soumission",
         "url": "https://books.toscrape.com/catalogue/soumission_998/index.html",
         "selector_price": "p.price_color",
-        "selector_stock": "p.instock.availability"
+        "selector_stock": "p.instock.availability",
+        "threshold" : 50.00
     }
 ]
 
+SMTP_SERVER = "://gmail.com"
+SMTP_PORT = 587
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 # main.py
 import time
 import logging
-from config import TARGET_PRODUCTS, HEADERS
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from config import TARGET_PRODUCTS, HEADERS, SMTP_PORT, SMTP_SERVER
 from database import init_db, log_price_data
 from scraper import scrape_product
+
+def send_email_alert(product, current, target):
+    sender_pass = os.environ.get("EMAIL_PASS")
+    sender_email = os.environ.get("EMAIL_USER")
+    receiver_email = os.environ.get("EMAIL_RECEIVER")
+    print(sender_email, sender_pass, receiver_email)
+    if not sender_email or not sender_pass or not receiver_email:
+        print("Email params not complete. Skipping alerts.")
+        return
+    
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = f"Price Drop Alert: {product}"
+
+    body = f"""
+        The price monitoring tool has detected an active shift in prices!
+
+        Product: {product}
+        Current Price: £{current}
+        Your Target Alert Threshold: £{target}
+        
+        Please log in to your dashboard to review historical analysis.
+"""
+    msg.attach(MIMEText(body, "plain"))
+    try:
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(sender_email, sender_pass)
+        server.send_message(msg)
+        server.quit()
+        logging.info(f"📩 Price alert successfully dispatched to {receiver_email}!")
+    except Exception as e:
+        logging.error(f"Failed to transmit automated alert notification: {str(e)}")
+
 
 def run_pipeline():
     logging.info("Starting Competitor Price Monitoring Pipeline...")
@@ -26,7 +65,9 @@ def run_pipeline():
                 is_in_stock=data["is_in_stock"]
             )
             logging.info(f"Successfully logged {data['name']}: £{data['price']} (In Stock: {data['is_in_stock']})")
-        
+            if data["price"] < product["threshold"]:
+                logging.info(f"🔥 Price drop detected for {data['name']}! Initiating alert...")
+                send_email_alert(data["name"], data["price"], product["threshold"])
         # Polite crawling rule: pause between requests to avoid getting banned
         time.sleep(2)
         
